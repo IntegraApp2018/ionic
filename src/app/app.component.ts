ca19b3306c392a851bd880769cf89ed61366d060
Lucas Gomes <gomes@200-146-253-050.xd-dynamic.ctbcnetsuper.com.br>
2018-09-02 14:30:57 -0300

    profile added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
 import { LoginPage } from '../pages/login/login';
+import { PerfilPage } from '../pages/perfil/perfil';
+import { HomePage } from '../pages/home/home';
 
 @Component({
   templateUrl: 'app.html'
